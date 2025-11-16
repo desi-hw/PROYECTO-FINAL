026de5ddab192cd67e578d3d6a68b5ce30fc09a4
desi-hw <desireewasszabo@gmail.com>
2025-11-15 21:57:56 -0600

    Create examen.c

---
+++ examen.c
@@ -0,0 +1,20 @@
+/*Huitrón Was-Szabo Elizabeth Desireé
+Proyecto Final*/
+#include<stdio.h>
+void leerAlmacenarVotos();
+void calcularVotos();
+void reparticionDePuntos();
+int main()
+{
+  int i;
+  printf("\tConcurso de Canciones \n");
+  printf("Diez personas deben votar por 3 canciones que mas les gusten \n");
+  printf("Las canciones son: \n");
+  for(i=0; i<10; i++)
+  {
+    printf("Cancion %d \n", i);
+  }
+}
+void leerAlmacenarVotos()
+{ 
+}
